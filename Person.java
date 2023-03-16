5e7a925708ea033f9a458146c345d803d9d89513
Galina3 <126572102+Galina3@users.noreply.github.com>
2023-03-16 21:57:41 +0100

    Add files via upload

---
+++ Person.java
@@ -0,0 +1,12 @@
+public class Person {
+    public static void main(String[] args) {
+       String name = "Galina";
+       String lastName = "Sopitko";
+       int yearOfBirth = 1989;
+
+        System.out.println(name);
+        System.out.println( lastName);
+        System.out.println(yearOfBirth);
+
+    }
+}
